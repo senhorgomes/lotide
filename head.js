cea096fd7d4d5aa0b5c0c1fe82ede1f620529094
Bryan Gomes <bryanpgomes@gmail.com>
2020-05-25 23:28:24 +0000

    Utitlization of _.head()

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+const head = function(array) {
+  let first = array[0]
+  return first
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
