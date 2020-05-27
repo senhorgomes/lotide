623f0f53efbc9b26f73dac7f2462f18038fcef29
Bryan Gomes <bryanpgomes@gmail.com>
2020-05-27 23:14:18 +0000

    Finding a Key using a Value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const findKeyByValue = function(object, keyValue) {
+  for (let value in object) {
+    let key = value.toString()
+    if (object[key] === keyValue) {
+      return key
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
