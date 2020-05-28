7ca7f4bc5d52b0b1ad90af7ab6a1d8545113118f
Bryan Gomes <bryanpgomes@gmail.com>
2020-05-28 00:45:32 +0000

    compare two objects

---
+++ eqObjects.js
@@ -0,0 +1,58 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let keyObj1 in object1) {
+    for (let keyObj2 in object2) {
+      if (keyObj1 === keyObj2) {
+        if (Array.isArray(object1[keyObj1])) {
+          if (Array.isArray(object2[keyObj2])) {
+            if (!eqArrays(object1[keyObj1], object2[keyObj2])) {
+              return false
+            }
+          }
+        } else {
+          if (object1[keyObj1] !== object2[keyObj2]) {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
