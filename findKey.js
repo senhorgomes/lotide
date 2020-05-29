521b93e57ae84177a9ca1ce30c7146bf162b02ed
Bryan Gomes <bryanpgomes@gmail.com>
2020-05-29 01:48:00 +0000

    Find the object that matches the first key

---
+++ findKey.js
@@ -0,0 +1,58 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key
+    }
+  }
+}
+//
+//let bestMovies = [];
+//movieArr.forEach(movie => {
+//  if (callback(movie)) {
+//    bestMovies.push(movie.title)
+//  }
+//});
+//return bestMovies;
+//for (let input of array) {
+//  if (callback(input)) {
+//    return array.slice(0, array.indexOf(input));
+//  }
+//}
+//
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+
+assertEqual(findKey({
+  "The Matrix": { personalRating: 4 },
+  "Avengers":   { personalRating: 3 },
+  "Joe Dirt":      { personalRating: 2 },
+  "Night at the Roxbury":   { personalRating: 5 },
+  "Akira":       { personalRating: 4 },
+  "Cabin in The Woods":  { personalRating: 3 },
+  "Blade Runner":  { personalRating: 5 }
+}, x => x.personalRating === 5), "Night at the Roxbury");
