ef4d2a3b168cc2d4dbfef6e7c2773338203d6151
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-01 21:52:40 +0000

    Refactored middle.js, I think it can be refactored even more.

--- middle.js
+++ middle.js
@@ -1,28 +1,4 @@
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-    }
-  }
-  return (`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-};
-/*const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};*/
-
-// ACTUAL FUNCTION
-const middle = function(array) {
+const middle = (array => {
   const midOfArray = [];
   //check if array length is more than two, if so, run middle, if not return middleNumbers
   if (array.length > 2) {
@@ -39,17 +15,6 @@ const middle = function(array) {
     }
   }
   return midOfArray;
-};
+});
 
-// TEST CODE
-// ...
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-console.log(assertArraysEqual(middle([1]), [])); // = True
-console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // = True
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); // = True
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3])); // = False
+module.exports = middle;
