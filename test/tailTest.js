f84e9c864edc65643d049caa4b15afaa296befaf
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-01 22:58:13 +0000

    Chai is fully implemented

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,12 +3,7 @@ const assert = require('chai').assert;
 
 // Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-//assertEqual(result.length, 2); // ensure we get back two elements
-//assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-//assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-//
-
-// Test Case: Check the original array
+// Test Case: Check if the original array is not modified
 const blank = tail([]);
 tail(blank); // no need to capture the return value since we are not checking it
 //Check is array has been modified
@@ -26,6 +21,9 @@ describe("#tail", () => {
     assert.strictEqual(result.length, 2); // ensure we get back a length of two after removing only the first element
   });
   it("returns 'Lighthouse' when prompted to return index 0 of trimmed array.", () => {
-    assert.strictEqual(result.length, 2); // ensure the trimmed array
+    assert.strictEqual(result[0], "Lighthouse"); // ensure the trimmed array returns an array with everything except the first element
+  });
+  it("returns 'Labs' when prompted to return index 1 of trimmed array.", () => {
+    assert.strictEqual(result[1], "Labs"); // ensure the trimmed array returns an array with everything except the first element
   });
 });
