42d3cec80bbd4af2cbe6aacbc7f44e792de27101
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-01 23:11:09 +0000

    All tests added, refactored with chai.

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,12 +2,8 @@ const assert = require('chai').assert;
 const middle = require("../middle");
 
 // TEST CODE
-// ...
-
-
-//console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // = True
-//console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); // = True
-//console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])); // = true
+// Tests are pretty self explanitory, it runs through the array and finds the middle element
+// If the element is even, return the two middle elements.
 
 describe("#middle", () => {
   it("returns [] for [1]", () => {
@@ -18,5 +14,8 @@ describe("#middle", () => {
   });
   it("returns 3 for middle of [1, 2, 3, 4, 5]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns 3, 4 for middle of [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 });
