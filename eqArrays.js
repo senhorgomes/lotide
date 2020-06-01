567537ebf29be3baa5c65c8352a6292b3f97eb41
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-01 20:40:28 +0000

    Refractored code to make it simpler. Exported tail & eqArrays. Made seperate test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-const eqArrays = function(array1, array2) {
+const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -18,10 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // =  true
-console.log(eqArrays([1, 2, 3], [3, 2, 1, 0])); // =  false
-console.log(eqArrays([1, 2, 3], [1, 2, "3"])); // = false
+module.exports = eqArrays;
