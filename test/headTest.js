7cce6507d20420069ae27157577c7b11b8604e0c
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-01 23:00:48 +0000

    Added more comments

--- test/headTest.js
+++ test/headTest.js
@@ -4,7 +4,7 @@ const head = require("../head")
 
 //assertEqual(head([5,6,7]), 5);
 //assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
+//TEST CODE
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
