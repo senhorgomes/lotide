86d6fd32f7c03813831ea6c79fd6f3a56ac19e58
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-01 20:54:05 +0000

    added an assertArrayEqualTest, requires assertArrayEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // =  true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1, 0])); // =  false
+console.log(assertArraysEqual([1, 2, 3], [1, 2, "3"])); // = false
