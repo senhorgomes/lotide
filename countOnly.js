57f8c70c22558e9875fabe81e22cd0188f221a99
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-02 00:11:07 +0000

    Removed assertEqual, added comments to explain function, and blanked out test samples for later use

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
@@ -14,8 +5,8 @@ const countOnly = function(allItems, itemsToCount) {
 
   for (const item of allItems) {
     console.log(item);
-    if (itemsToCount[item]) { 
-      if (results[item]) {
+    if (itemsToCount[item]) { //Same thing as count letters, but instead of looking for spaces
+      if (results[item]) {    // it checks to see if the 'itemsToCount' is there
         results[item] += 1;
       } else {
         results[item] = 1;
@@ -26,7 +17,9 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
+module.exports= countOnly;
+//Test Samples
+/*const firstNames = [
   "Karl",
   "Salima",
   "Agouhanna",
@@ -42,4 +35,4 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
+assertEqual(result1["Fang"], 2);*/
