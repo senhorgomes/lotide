506c2c472af3e4e0c3d7bdc74c1d016cc77fae40
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-02 00:33:45 +0000

    Added export, eliminated assert and added comments

--- letterPosition.js
+++ letterPosition.js
@@ -1,45 +1,20 @@
-/*
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-*/
-
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-    }
-  }
-  return (`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-};
-
-const letterPositions = function(sentence) {
+const letterPosition = function(sentence) {
   const results = {};
   // logic to update results here
   for (i = 0; i < sentence.length; i++) {
     if (sentence[i] !== " ") {
       let letter = sentence[i]
-      if (results[letter]) {
-        results[letter].push(i);
+      if (results[letter]) { //This solution used a similar method of countLetter, but instead uses an array to show which index position it is
+        results[letter].push(i); // if the array already exists, push the new position into that array
       } else {
-        results[letter] = [i];
+        results[letter] = [i]; //else create a key with that letter and an array that show which position the letter is in
       };
     };
   };
   return results;
 };
 
-console.log(letterPositions("hello"));
-console.log(letterPositions("lighthouse in the house"));
-assertArraysEqual(letterPositions("hello", "e"), [1]);
+module.exports = letterPosition;
+//console.log(letterPositions("hello"));
+//console.log(letterPositions("lighthouse in the house"));
+//assertArraysEqual(letterPositions("hello", "e"), [1]);
