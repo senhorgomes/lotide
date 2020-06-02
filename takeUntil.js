13c7c612cbd18ed762fd55e31389d8ad45eaa128
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-02 00:41:19 +0000

    Added export, removed assertEq, and explained the function in more detail

--- takeUntil.js
+++ takeUntil.js
@@ -1,17 +1,5 @@
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return (`🚫🚫🚫 Assertion Failed: ${array1} !== ${array2}`);
-    }
-  }
-  return (`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-};
-
 const takeUntil = function(array, callback) {
-  // ...
+  // This function goes through an array, until a certain point, and provides a new array from when it cut off
   for (let input of array) {
     if (callback(input)) {
       return array.slice(0, array.indexOf(input));
@@ -19,7 +7,8 @@ const takeUntil = function(array, callback) {
   }
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+module.exports = takeUntil;
+/*const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 
@@ -30,4 +19,4 @@ const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
 console.log(assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]));
-console.log(assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]));
+console.log(assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]));*/
