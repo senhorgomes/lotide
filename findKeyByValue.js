0e55828a930cd935c499bc0fe827c089f876413e
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-02 00:29:08 +0000

    Added export and comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,22 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
 const findKeyByValue = function(object, keyValue) {
   for (let value in object) {
     let key = value.toString()
-    if (object[key] === keyValue) {
+    if (object[key] === keyValue) { //Same thing as findKey, but this one lets you search up, using words, the value and it returns the key.
       return key
     }
   }
 };
 
-const bestTVShowsByGenre = { 
+module.exports = findKeyByValue;
+/*const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
@@ -25,4 +17,4 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))*/
