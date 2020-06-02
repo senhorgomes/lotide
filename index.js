0d5e747e4210063337fa8816d38424a02f9e7e6c
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-02 00:34:52 +0000

    Added more imports and exports

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // index.js
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual')
-const countLetters = require('./countLetters')
-const head   = require('./head');
-const tail   = require('./tail');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const head = require('./head');
+const letterPosition = require('./letterPosition');
+const  = require('./');
+const  = require('./');
+const  = require('./');
+const tail = require('./tail');
 const middle = require('./middle');
 
 module.exports = {
@@ -12,7 +21,14 @@ module.exports = {
   assertEqual: assertEqual,
   assertObjectsEqual: assertObjectsEqual,
   countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
   head: head,
+  letterPosition: letterPosition,
+  
   tail: tail,
   middle: middle
 };
